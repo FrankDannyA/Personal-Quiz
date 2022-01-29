38d8ff05812b4c4b631583ce213e968f080881b8
Daniil <orgfrank@icloud.com>
2022-01-29 19:33:04 +0600

    finish

--- Personal Quiz/Controller/ResultViewController.swift
+++ Personal Quiz/Controller/ResultViewController.swift
@@ -8,14 +8,45 @@
 import UIKit
 
 class ResultViewController: UIViewController {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var resultAnswersLabel: UILabel!
+    @IBOutlet weak var resultDefenitionLabel: UILabel!
+    
+    //MARK: - Public properties
+    var responses: [Answer]!
 
+    //MARK: - Override func
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        updateResult()
     }
     
+    //MARK: - Private func
     private func setupUI(){
         navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.hidesBackButton = true
+    }
+    
+    private func updateResult(){
+        var frequencyOfAnimals: [AnimalType: Int] = [:]
+        let animals = responses.map{ $0.type}
+        
+        for animal in animals {
+            frequencyOfAnimals[animal] = (frequencyOfAnimals[animal] ?? 0) + 1
+        }
+        
+        let sortedFrequencyOfAnimals = frequencyOfAnimals.sorted { $0.value > $1.value}
+        
+        guard let mostFrequencyOfAnimals = sortedFrequencyOfAnimals.first?.key else { return }
+        
+        updateUI(with: mostFrequencyOfAnimals)
+    }
+    
+    private func updateUI(with animal: AnimalType){
+        resultAnswersLabel.text = "Вы - \(animal.rawValue)"
+        resultDefenitionLabel.text = "\(animal.divinition)"
     }
 
 }
